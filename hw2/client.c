61d631b732cb3a4084b00bd9342c2a870b909fed
dethakur <devashish.thakur11@gmail.com>
2015-10-20 19:56:53 -0400

    Basic UDP

--- hw2/client.c
+++ hw2/client.c
@@ -1,3 +1,43 @@
-int main(){
-return 0;
+#include "unp.h"
+void dg_cli_new(int,SA*,socklen_t);
+int main(int argc,char** argv){
+	int sockfd;
+	struct sockaddr_in servaddr;
+
+	sockfd = Socket(AF_INET,SOCK_DGRAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(5589);
+
+	inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr);
+
+	sockfd = socket(AF_INET,SOCK_DGRAM,0);
+	dg_cli_new(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	return 0;
+}
+void dg_cli_new(int sockfd, SA* servaddr,socklen_t servlen){
+	int n;
+	char sendline[MAXLINE],recvline[MAXLINE];
+
+	strcpy(sendline,"chutiya");
+	printf("Comes here !?!\n");
+
+
+
+
+	Connect(sockfd,(SA*)servaddr,servlen);
+
+	while(1){
+		printf("Sending = %s\n",sendline);
+		Write(sockfd,sendline,strlen(sendline));
+		printf("Packet send , now waiting for it!\n");
+		n = Read(sockfd,recvline,MAXLINE);
+		printf("Received = %s and read li\n",recvline);
+		bzero(recvline,sizeof(recvline));
+		sleep(1);
+
+	}
+
 }
--- hw2/server.c
+++ hw2/server.c
@@ -1,3 +1,32 @@
-int main(){
-return 0;
+#include "unp.h"
+
+void dg_server(int,SA*,socklen_t);
+int main(int argc,char* argv[]){
+	int sockfd;
+	struct sockaddr_in servaddr,cliaddr;
+
+	sockfd = Socket(AF_INET,SOCK_DGRAM,0);
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(5589);
+
+	bind(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	dg_server(sockfd,(SA*)&cliaddr,sizeof(cliaddr));
+	return 0;
+
+}
+void dg_server(int sockfd,SA* cliaddr,socklen_t clilen){
+	int n;
+	socklen_t len;
+	char msg[MAXLINE];
+	while(1){
+		len = clilen;
+		n = recvfrom(sockfd,msg,MAXLINE,0,cliaddr,&len);
+		printf("Received = %s \n",msg);
+		sendto(sockfd,msg,n,0,cliaddr,len);
+		bzero(msg,sizeof(msg));
+	}
 }
