2e9f874d933e54688dff527a2be08d9e7b5b6667
dethakur <devashish.thakur11@gmail.com>
2015-10-21 10:42:39 -0400

    UDP packet change

--- hw2/client.c
+++ hw2/client.c
@@ -1,5 +1,10 @@
 #include "unp.h"
-void dg_cli_new(int,SA*,socklen_t);
+//void dg_cli_new(int,SA*,socklen_t);
+void send_msg_test(int,SA*,socklen_t);
+
+static struct ack_data{
+	int val;
+}send_hdr;
 int main(int argc,char** argv){
 	int sockfd;
 	struct sockaddr_in servaddr;
@@ -13,31 +18,50 @@ int main(int argc,char** argv){
 	inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr);
 
 	sockfd = socket(AF_INET,SOCK_DGRAM,0);
-	dg_cli_new(sockfd,(SA*)&servaddr,sizeof(servaddr));
+//	dg_cli_new(sockfd,(SA*)&servaddr,sizeof(servaddr));
+	send_msg_test(sockfd,(SA*)&servaddr,sizeof(servaddr));
 
 	return 0;
 }
-void dg_cli_new(int sockfd, SA* servaddr,socklen_t servlen){
-	int n;
-	char sendline[MAXLINE],recvline[MAXLINE];
 
-	strcpy(sendline,"chutiya");
-	printf("Comes here !?!\n");
+void send_msg_test(int sockfd,SA* servaddr,socklen_t servlen){
+	struct iovec iovec_send[1];
+	struct msghdr msgsend;
+	bzero(&msgsend,sizeof(msgsend));
+	msgsend.msg_name = servaddr;
+	msgsend.msg_namelen = servlen;
+	msgsend.msg_iov = (void*)&iovec_send;
+	msgsend.msg_iovlen = 1;
 
+	send_hdr.val = 999;
 
+	iovec_send[0].iov_base = (void*)&send_hdr;
+	iovec_send[0].iov_len = sizeof(struct ack_data);
 
-
-	Connect(sockfd,(SA*)servaddr,servlen);
-
-	while(1){
-		printf("Sending = %s\n",sendline);
-		Write(sockfd,sendline,strlen(sendline));
-		printf("Packet send , now waiting for it!\n");
-		n = Read(sockfd,recvline,MAXLINE);
-		printf("Received = %s and read li\n",recvline);
-		bzero(recvline,sizeof(recvline));
-		sleep(1);
-
-	}
-
+	Sendmsg(sockfd,&msgsend,0);
 }
+//void dg_cli_new(int sockfd, SA* servaddr,socklen_t servlen){
+//	int n;
+//	char sendline[MAXLINE],recvline[MAXLINE];
+//
+//	strcpy(sendline,"chutiya");
+//	printf("Comes here !?!\n");
+//
+//
+//
+//
+//	Connect(sockfd,(SA*)servaddr,servlen);
+//
+//	while(1){
+//		printf("Sending = %s\n",sendline);
+//		Write(sockfd,sendline,strlen(sendline));
+//		printf("Packet send , now waiting for it!\n");
+//		n = Read(sockfd,recvline,MAXLINE);
+//		printf("Received = %s and read li\n",recvline);
+//		bzero(recvline,sizeof(recvline));
+//		sleep(1);
+//
+//
+//	}
+//
+//}
--- hw2/server.c
+++ hw2/server.c
@@ -1,6 +1,12 @@
 #include "unp.h"
 
-void dg_server(int,SA*,socklen_t);
+//void dg_server(int,SA*,socklen_t);
+void receive_msg_test(int,SA*,socklen_t);
+
+static struct ack_data{
+	int val;
+}receive_hdr;
+
 int main(int argc,char* argv[]){
 	int sockfd;
 	struct sockaddr_in servaddr,cliaddr;
@@ -13,20 +19,42 @@ int main(int argc,char* argv[]){
 	servaddr.sin_port = htons(5589);
 
 	bind(sockfd,(SA*)&servaddr,sizeof(servaddr));
+	receive_msg_test( sockfd,(SA*)&servaddr,sizeof(servaddr));
 
-	dg_server(sockfd,(SA*)&cliaddr,sizeof(cliaddr));
 	return 0;
 
 }
-void dg_server(int sockfd,SA* cliaddr,socklen_t clilen){
-	int n;
-	socklen_t len;
-	char msg[MAXLINE];
+
+void receive_msg_test(int sockfd,SA* servaddr,socklen_t servlen){
+	struct iovec iovec_send[1];
+	struct msghdr msgreceive;
+	bzero(&msgreceive,sizeof(msgreceive));
+	msgreceive.msg_name = servaddr;
+	msgreceive.msg_namelen = servlen;
+	msgreceive.msg_iov = iovec_send;
+	msgreceive.msg_iovlen = 1;
+
+//	receive_hdr.val = 999;
+
+	iovec_send[0].iov_base = (void*)&receive_hdr;
+	iovec_send[0].iov_len = sizeof(struct ack_data);
 	while(1){
-		len = clilen;
-		n = recvfrom(sockfd,msg,MAXLINE,0,cliaddr,&len);
-		printf("Received = %s \n",msg);
-		sendto(sockfd,msg,n,0,cliaddr,len);
-		bzero(msg,sizeof(msg));
+		Recvmsg(sockfd,&msgreceive,0);
+		printf("Message received %d !!\n",receive_hdr.val);
+//		sleep(1);
+
 	}
 }
+
+//void dg_server(int sockfd,SA* cliaddr,socklen_t clilen){
+//	int n;
+//	socklen_t len;
+//	char msg[MAXLINE];
+//	while(1){
+//		len = clilen;
+//		n = recvfrom(sockfd,msg,MAXLINE,0,cliaddr,&len);
+//		printf("Received = %s \n",msg);
+//		sendto(sockfd,msg,n,0,cliaddr,len);
+//		bzero(msg,sizeof(msg));
+//	}
+//}
