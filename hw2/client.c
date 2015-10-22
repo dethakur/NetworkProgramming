5cb5b6f3ba433067f4e2334a3b26d803869028bd
dethakur <devashish.thakur11@gmail.com>
2015-10-22 00:13:48 -0400

    Adding partial code

--- hw2/client.c
+++ hw2/client.c
@@ -1,45 +1,178 @@
 #include "unp.h"
+#include <unistd.h>
+#include "unpthread.h"
+#include <string.h>
+#define RWND_SIZE 10
 //void dg_cli_new(int,SA*,socklen_t);
-void send_msg_test(int,SA*,socklen_t);
+int send_file_name(int,SA*,socklen_t);
+void send_ack(void*, socklen_t,int);
+void receive_file_data(int,void*,socklen_t,char*);
+void buffer_reader_thread();
+void push_data_to_buffer(char*);
+int get_window_size();
 
-static struct ack_data{
-	int val;
-}send_hdr;
+typedef enum {ack,ping,data} data_type;
+
+//UDP Packet sent across along with data
+static struct udp_data{
+	uint32_t seq;
+	uint32_t ts;
+	uint32_t rwnd;
+	data_type type;
+}send_hdr,recv_hdr;
+
+
+//Link List of Buffers
+struct buffer{
+	char data[MAXLINE];
+	uint32_t is_filled;
+	struct buffer *next;
+};
+
+struct buffer head[RWND_SIZE];
+
+
+
 int main(int argc,char** argv){
-	int sockfd;
-	struct sockaddr_in servaddr;
 
-	sockfd = Socket(AF_INET,SOCK_DGRAM,0);
+	int sockfd;
+	struct sockaddr_in servaddr,cliaddr,sock_addr;
 
 	bzero(&servaddr,sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(5589);
 
+	bzero(&cliaddr,sizeof(cliaddr));
+	cliaddr.sin_family = AF_INET;
+	cliaddr.sin_port = htons(0);
+
 	inet_pton(AF_INET,"127.0.0.1",&servaddr.sin_addr);
 
+	//	sockfd = socket(AF_INET,SOCK_DGRAM,0);
 	sockfd = socket(AF_INET,SOCK_DGRAM,0);
-//	dg_cli_new(sockfd,(SA*)&servaddr,sizeof(servaddr));
-	send_msg_test(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	int i=0;
+	for(i=0;i<RWND_SIZE;i++){
+		head[i].is_filled = -1;
+	}
+
+	bind(sockfd,(SA*)&cliaddr,sizeof(cliaddr));
+
+	bzero(&sock_addr,sizeof(sock_addr));
+	socklen_t len = sizeof(struct sockaddr_in);
+	getsockname(sockfd, (SA*)&sock_addr,&len);
+	printf("CLient port = %d\n",ntohs(sock_addr.sin_port));
+
+	Connect(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	struct sockaddr_in peer_sock;
+	bzero(&peer_sock,sizeof(peer_sock));
+	socklen_t len2 = sizeof(peer_sock);
+
+
+	getpeername(sockfd,(SA*)&peer_sock,&len2);
+	printf("Server port = %d\n",ntohs(peer_sock.sin_port));
+
+	char sendline[MAXLINE],recvline[MAXLINE];
+
+	strcpy(sendline,"chutiya");
+	printf("Comes here !?!\n");
+	//	send_file_name(sockfd,(SA*)&servaddr,sizeof(servaddr));
+	Write(sockfd,sendline,strlen(sendline));
+	char buf[MAXLINE];
+	receive_file_data(sockfd,NULL,0,&buf);
+	printf("Data Received 1 = %s",buf);
+	receive_file_data(sockfd,NULL,0,&buf);
+	char *ptr;
+	int new_port_number = strtol(buf,ptr,10);
+	printf("New port number = %d\n",new_port_number);
+
+	while(1){
+		bzero(&buf,sizeof(buf));
+		receive_file_data(sockfd,NULL,0,&buf);
+		printf("Data Received = %s\n",buf);
+	}
+
+	close(sockfd);
+	printf("Writinf done!!\n");
 
 	return 0;
+
 }
 
-void send_msg_test(int sockfd,SA* servaddr,socklen_t servlen){
-	struct iovec iovec_send[1];
-	struct msghdr msgsend;
-	bzero(&msgsend,sizeof(msgsend));
-	msgsend.msg_name = servaddr;
-	msgsend.msg_namelen = servlen;
-	msgsend.msg_iov = (void*)&iovec_send;
-	msgsend.msg_iovlen = 1;
 
-	send_hdr.val = 999;
+
+void receive_file_data(int sockfd,void* servaddr, socklen_t servlen,char* buf){
+	struct msghdr msgrecv;
+//	char send_buf[MAXLINE];
+	struct iovec iovec_send[2];
+
+	msgrecv.msg_name = NULL;
+	msgrecv.msg_namelen = 0;
 
 	iovec_send[0].iov_base = (void*)&send_hdr;
-	iovec_send[0].iov_len = sizeof(struct ack_data);
+	iovec_send[0].iov_len = sizeof(struct udp_data);
+	iovec_send[1].iov_base = (void*)buf;
+	iovec_send[1].iov_len = MAXLINE;
+	msgrecv.msg_iovlen = 2;
+	msgrecv.msg_iov = (void*)&iovec_send;
 
-	Sendmsg(sockfd,&msgsend,0);
+
+//	bzero(send_buf,sizeof(send_buf));
+//	printf("Trying again!!\n");
+
+	Recvmsg(sockfd,&msgrecv,0);
+	if(send_hdr.type == ack){
+		printf("Acknowledgement received\n");
+	}
+
+	//		push_data_to_buffer(send_buf);
+	//		send_ack(servaddr,servlen,sockfd);
+	//		printf("[Client] Ack sent to the server!!\n");
+
+	//	}
 }
+
+void push_data_to_buffer(char* send_buf){
+	int i=0;
+	for(i=0;i<RWND_SIZE;i++){
+		if(head[i].is_filled == -1){
+			strcpy(head[i].data,send_buf);
+			head[i].is_filled = 1;
+			printf("[Data] Pushed to buffer");
+			break;
+		}
+	}
+}
+int get_window_size(){
+	int count = 0;
+	int i=0;
+	for(i=0;i<RWND_SIZE;i++){
+		if(head[i].is_filled == -1){
+			count++;
+		}
+	}
+	return count;
+}
+void send_ack(void* addr, socklen_t len,int sockfd){
+	struct udp_data ack_data;
+	struct iovec iovec_send[1];
+	struct msghdr msgrecv;
+	bzero(&msgrecv,sizeof(msgrecv));
+
+	ack_data.type=ack;
+	ack_data.rwnd = get_window_size();
+	msgrecv.msg_name = addr;
+	msgrecv.msg_namelen = len;
+
+	iovec_send[0].iov_base = (void*)&ack_data;
+	iovec_send[0].iov_len = sizeof(struct udp_data);
+	msgrecv.msg_iov = (void*)&iovec_send;
+	msgrecv.msg_iovlen = 1;
+
+	Sendmsg(sockfd,&msgrecv,0);
+}
+//
 //void dg_cli_new(int sockfd, SA* servaddr,socklen_t servlen){
 //	int n;
 //	char sendline[MAXLINE],recvline[MAXLINE];
@@ -64,4 +197,49 @@ void send_msg_test(int sockfd,SA* servaddr,socklen_t servlen){
 //
 //	}
 //
+//}
+
+//int send_file_name(int sockfd,SA* servaddr,socklen_t servlen){
+//	struct iovec iovec_send[2],iovec_recv[2];
+//	struct msghdr msgsend,msgrecv;
+//	char send_buf[MAXLINE],recv_buf[MAXLINE];
+//
+//	strcpy(send_buf,"file_test");
+//	bzero(&msgsend,sizeof(msgsend));
+//	send_hdr.type = data;
+//
+//	msgsend.msg_name = servaddr;
+//	msgsend.msg_namelen = servlen;
+//
+//	iovec_send[0].iov_base = (void*)&send_hdr;
+//	iovec_send[0].iov_len = sizeof(struct udp_data);
+//	iovec_send[1].iov_base = (void*)&send_buf;
+//	iovec_send[1].iov_len = sizeof(send_buf);
+//	msgsend.msg_iovlen = 2;
+//	msgsend.msg_iov = (void*)&iovec_send;
+//
+//	bzero(&msgrecv,sizeof(msgrecv));
+//	msgrecv.msg_name = NULL;
+//	msgrecv.msg_namelen = 0;
+//	msgrecv.msg_iov = (void*)&iovec_recv;
+//
+//	msgrecv.msg_name = servaddr;
+//	msgrecv.msg_namelen = servlen;
+//
+//	iovec_recv[0].iov_base = (void*)&send_hdr;
+//	iovec_recv[0].iov_len = sizeof(struct udp_data);
+//	iovec_recv[1].iov_base = (void*)&recv_buf;
+//	iovec_recv[1].iov_len = sizeof(recv_buf);
+//	msgrecv.msg_iov = (void*)&iovec_recv;
+//	msgrecv.msg_iovlen = 2;
+//
+//
+//	Sendmsg(sockfd,&msgsend,0);
+//	Recvmsg(sockfd,&msgrecv,0);
+//	if(recv_hdr.type == ack){
+//		printf("[Client]Ack received From server. Lets now wait for data transfer. \n");
+//		//		receive_file_data(sockfd,msgrecv.msg_name,msgrecv.msg_namelen);
+//		return 1;
+//	}
+//	return -1;
 //}
--- hw2/server.c
+++ hw2/server.c
@@ -1,11 +1,23 @@
-#include "unp.h"
+#include "unpthread.h"
+#include	<time.h>
+#include <fcntl.h>
+#include <sys/file.h>
 
 //void dg_server(int,SA*,socklen_t);
-void receive_msg_test(int,SA*,socklen_t);
+void receive_filename(int);
+void send_ack(void*, socklen_t,int);
+void send_data(int,char*);
+void udp_reliable_transfer(int);
 
-static struct ack_data{
-	int val;
-}receive_hdr;
+typedef enum {ack,ping,data} data_type;
+static struct udp_data{
+	uint32_t seq;
+	uint32_t ts;
+	uint32_t rwnd;
+	data_type type;
+}send_hdr,recv_hdr;
+
+struct sockaddr_in cliaddr;
 
 int main(int argc,char* argv[]){
 	int sockfd;
@@ -18,43 +30,141 @@ int main(int argc,char* argv[]){
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(5589);
 
-	bind(sockfd,(SA*)&servaddr,sizeof(servaddr));
-	receive_msg_test( sockfd,(SA*)&servaddr,sizeof(servaddr));
+	Bind(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	fd_set rset;
+	char buf[MAXLINE];
+	while(1){
+		FD_ZERO(&rset);
+		FD_SET(sockfd,&rset);
+		int maxfd1 = sockfd+1;
+		printf("Waiting for select! \n");
+		Select(maxfd1,&rset,NULL,NULL,NULL);
+		if(FD_ISSET(sockfd,&rset)){
+			printf("Select got broken! which means socket got interrupted \n");
+			receive_filename(sockfd);
+			pid_t pid = fork();
+			if(pid == 0){
+				udp_reliable_transfer(sockfd);
+			}else{
+				continue;
+			}
+		}
+	}
+
+
+
+//	receive_filename( sockfd,(SA*)&servaddr,sizeof(servaddr));
 
 	return 0;
 
 }
 
-void receive_msg_test(int sockfd,SA* servaddr,socklen_t servlen){
-	struct iovec iovec_send[1];
-	struct msghdr msgreceive;
-	bzero(&msgreceive,sizeof(msgreceive));
-	msgreceive.msg_name = servaddr;
-	msgreceive.msg_namelen = servlen;
-	msgreceive.msg_iov = iovec_send;
-	msgreceive.msg_iovlen = 1;
+void udp_reliable_transfer(int sockfd){
+	int new_sockfd;
+	struct sockaddr_in servaddr,sock_addr;
+	socklen_t len2 = sizeof(sock_addr);
 
-//	receive_hdr.val = 999;
+	bzero(&sock_addr,sizeof(sock_addr));
+	bzero(&servaddr,sizeof(servaddr));
+	new_sockfd = Socket(AF_INET,SOCK_DGRAM,0);
 
-	iovec_send[0].iov_base = (void*)&receive_hdr;
-	iovec_send[0].iov_len = sizeof(struct ack_data);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(0);
+
+	Bind(new_sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+	getsockname(new_sockfd,(SA*)&sock_addr,&len2);
+
+//	Connect(new_sockfd,(SA*)&cliaddr,sizeof(cliaddr));
+
+	int port_number = ntohs(sock_addr.sin_port);
+	printf("Connected Port Number = %d\n",port_number);
+
+	char buf[MAXLINE];
+	sprintf(buf, "%d", port_number);
+	printf("Data send = %s\n",buf);
+
 	while(1){
-		Recvmsg(sockfd,&msgreceive,0);
-		printf("Message received %d !!\n",receive_hdr.val);
-//		sleep(1);
 
+//		strcpy(buf,"Test1111");
+		send_data(sockfd,(char*)&buf);
+		sleep(2);
 	}
 }
 
-//void dg_server(int sockfd,SA* cliaddr,socklen_t clilen){
-//	int n;
-//	socklen_t len;
-//	char msg[MAXLINE];
-//	while(1){
-//		len = clilen;
-//		n = recvfrom(sockfd,msg,MAXLINE,0,cliaddr,&len);
-//		printf("Received = %s \n",msg);
-//		sendto(sockfd,msg,n,0,cliaddr,len);
-//		bzero(msg,sizeof(msg));
-//	}
+void receive_filename(int sockfd){
+	socklen_t clilen = sizeof(cliaddr);
+
+	struct iovec iovec_send[2],iovec_recv[1];
+	struct msghdr msgrecv;
+	char send_buf[MAXLINE],recv_buf[MAXLINE];
+
+	bzero(&msgrecv,sizeof(msgrecv));
+	msgrecv.msg_iov = (void*)&iovec_recv;
+
+	msgrecv.msg_name = (SA*)&cliaddr;
+	msgrecv.msg_namelen = clilen;
+
+	iovec_recv[0].iov_base = (void*)&recv_buf;
+	iovec_recv[0].iov_len = sizeof(recv_buf);
+	msgrecv.msg_iov = (void*)&iovec_recv;
+	msgrecv.msg_iovlen = 1;
+
+
+	recvmsg(sockfd,&msgrecv,0);
+	printf("[Server][receive_filename] Received = %s\n",recv_buf);
+	printf("Sending acknowledgement \n");
+	if(msgrecv.msg_name == NULL){
+		printf("Null Input \n");
+	}else{
+		printf("Non Null Input \n");
+//		Sendmsg(sockfd,&msgrecv,0);
+		send_ack(msgrecv.msg_name,msgrecv.msg_namelen,sockfd);
+	}
+
+
+
+}
+void send_ack(void* addr, socklen_t len,int sockfd){
+	struct udp_data ack_data;
+	struct iovec iovec_send[1];
+	struct msghdr msgrecv;
+	bzero(&msgrecv,sizeof(msgrecv));
+
+	ack_data.type=ack;
+	msgrecv.msg_name = addr;
+	msgrecv.msg_namelen = len;
+
+	iovec_send[0].iov_base = (void*)&ack_data;
+	iovec_send[0].iov_len = sizeof(struct udp_data);
+	msgrecv.msg_iov = (void*)&iovec_send;
+	msgrecv.msg_iovlen = 1;
+
+	Sendmsg(sockfd,&msgrecv,0);
+}
+//void wait_for_ack(void* addr,socklen_t,int sockfd){
+//
 //}
+void send_data(int sockfd,char* buf){
+	printf("[Server] Sending data to the client \n");
+	socklen_t clilen = sizeof(cliaddr);
+	struct udp_data ack_data;
+	struct iovec iovec_send[2];
+	struct msghdr msgrecv;
+	bzero(&msgrecv,sizeof(msgrecv));
+
+	ack_data.type=data;
+	msgrecv.msg_name = (SA*)&cliaddr;
+	msgrecv.msg_namelen = clilen;
+
+	iovec_send[0].iov_base = (void*)&ack_data;
+	iovec_send[0].iov_len = sizeof(struct udp_data);
+	iovec_send[1].iov_base = (void*)buf;
+	iovec_send[1].iov_len = sizeof(char)*strlen(buf);
+	msgrecv.msg_iov = (void*)&iovec_send;
+	msgrecv.msg_iovlen = 2;
+
+	Sendmsg(sockfd,&msgrecv,0);
+}
