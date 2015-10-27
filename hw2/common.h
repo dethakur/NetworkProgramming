ef091bc570ca7cca8f29f95375631d826c895af9
dethakur <devashish.thakur11@gmail.com>
2015-10-26 23:20:49 -0400

    Merged

--- hw2/common.h
+++ hw2/common.h
@@ -1,20 +1,22 @@
-typedef enum {ack,ping,data} data_type;
+typedef enum {
+	ack, ping, data
+} data_type;
 
 //UDP Packet sent across along with data
-struct udp_data{
+struct udp_data {
 	uint32_t seq;
 	uint32_t ts;
 	uint32_t rwnd;
 	data_type type;
 };
 
-struct iAddr{
+struct iAddr {
 	char ip_addr[MAXLINE];
 	char mask[MAXLINE];
 };
 typedef struct iAddr iAddr;
 
-struct query_obj{
+struct query_obj {
 	struct msghdr msgdata;
 	struct udp_data config;
 	char buf[MAXLINE];
@@ -24,10 +26,12 @@ struct query_obj{
 
 typedef struct query_obj query_obj;
 
-void init_query_obj(void* ,socklen_t ,struct query_obj* );
+void init_query_obj(void*, socklen_t, struct query_obj*);
 
-ssize_t recv_udp_data(int sockfd,void*,socklen_t,struct query_obj*);
-void send_udp_data(int sockfd,void*,socklen_t,struct query_obj*);
+void recv_udp_data(int sockfd, void*, socklen_t, struct query_obj*);
+void send_udp_data(int sockfd, void*, socklen_t, struct query_obj*);
 int get_addr_count();
-void fill_addr_contents(iAddr *addr);
+void fill_addr_contents(iAddr*);
+void disp_addr_contents(iAddr*, int);
+int check_addr_local(char*, iAddr*, int);
 
