944d4d79aa6f713eb0c0c642905f5a04512f96fa
dethakur <devashish.thakur11@gmail.com>
2015-10-26 23:21:18 -0400

    Merged

--- hw2/common.c
+++ hw2/common.c
@@ -32,10 +32,10 @@ void send_udp_data(int sockfd, void* servaddr, socklen_t servlen,
 	Sendmsg(sockfd, &obj->msgdata, 0);
 }
 
-ssize_t recv_udp_data(int sockfd, void* servaddr, socklen_t servlen,
+void recv_udp_data(int sockfd, void* servaddr, socklen_t servlen,
 		struct query_obj* obj) {
 	init_query_obj(servaddr, servlen, obj);
-	return Recvmsg(sockfd, &obj->msgdata, 0);
+	Recvmsg(sockfd, &obj->msgdata, 0);
 }
 int get_addr_count() {
 	struct ifi_info *ifi, *ifihead;
@@ -47,15 +47,55 @@ int get_addr_count() {
 	family = AF_INET;
 
 	int count = 0;
-	for (ifihead = ifi = Get_ifi_info_plus(AF_INET, 0); ifi != NULL; ifi
-			= ifi->ifi_next) {
-		printf("%s: \n", ifi->ifi_name);
+	for (ifihead = ifi = Get_ifi_info_plus(AF_INET, 0); ifi != NULL;
+			ifi = ifi->ifi_next) {
+//		printf("%s: \n", ifi->ifi_name);
 		count++;
 	}
 	free_ifi_info_plus(ifihead);
 	return count;
 }
 
+void disp_addr_contents(iAddr *addr, int count) {
+	int i = 0;
+	for (i = 0; i < count; i++) {
+		printf("IP address = %s and Mask = %s\n", addr[i].ip_addr,
+				addr[i].mask);
+	}
+}
+//Has to be corrected to longest prefix match
+int check_addr_local(char* address, iAddr* addr, int ip_addr_count) {
+	int i = 0;
+	struct sockaddr_in sock1;
+	bzero(&sock1, sizeof(struct sockaddr_in));
+	Inet_pton(AF_INET, address, &sock1.sin_addr);
+	int local = 0;
+	in_addr_t maxMask = 0;
+	for (i = 0; i < ip_addr_count; i++) {
+		struct sockaddr_in sock2, sock3;
+		bzero(&sock2, sizeof(struct sockaddr_in));
+		bzero(&sock3, sizeof(struct sockaddr_in));
+		Inet_pton(AF_INET, addr[i].ip_addr, &sock2.sin_addr);
+		Inet_pton(AF_INET, addr[i].mask, &sock3.sin_addr);
+		if ((sock1.sin_addr.s_addr & sock3.sin_addr.s_addr)
+				== (sock2.sin_addr.s_addr & sock3.sin_addr.s_addr)) {
+			if (local == 0) {
+				local = i;
+			}
+			if (sock3.sin_addr.s_addr > maxMask) {
+				maxMask = sock3.sin_addr.s_addr;
+				local = i;
+			}
+		}
+	}
+	if (local == 0) {
+		printf("Address NOT Local! \n");
+	}
+	if (local == 1) {
+		printf("Address is LOCAL\n");
+	}
+	return local;
+}
 void fill_addr_contents(iAddr *addr) {
 	struct ifi_info *ifi, *ifihead;
 	struct sockaddr *sa;
@@ -63,12 +103,12 @@ void fill_addr_contents(iAddr *addr) {
 	int i;
 	int count;
 	count = 0;
-	for (ifihead = ifi = Get_ifi_info_plus(AF_INET, 0); ifi != NULL; ifi
-			= ifi->ifi_next) {
+	for (ifihead = ifi = Get_ifi_info_plus(AF_INET, 0); ifi != NULL;
+			ifi = ifi->ifi_next) {
 //		printf("%s: ", ifi->ifi_name);
 		if ((sa = ifi->ifi_addr) != NULL) {
 			strcpy(addr[count].ip_addr, Sock_ntop_host(sa, sizeof(*sa)));
-			printf("IP Addr = %s\n",Sock_ntop_host(sa, sizeof(*sa)));
+//			printf("IP Addr = %s\n",Sock_ntop_host(sa, sizeof(*sa)));
 		}
 		if ((sa = ifi->ifi_ntmaddr) != NULL) {
 			strcpy(addr[count].mask, Sock_ntop_host(sa, sizeof(*sa)));
