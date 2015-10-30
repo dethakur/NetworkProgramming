e98ea06d6fb10ec1a9d43e5dd2b4447debbaddae
dethakur <devashish.thakur11@gmail.com>
2015-10-30 00:00:07 -0400

    client working

--- hw2/client.c
+++ hw2/client.c
@@ -16,7 +16,7 @@ void create_new_connection(int, char*);
 int min_seq_num();
 void send_ack_to_server(int, struct sockaddr_in, socklen_t, int, uint32_t,
 		int expected_seq);
-int same_host_name(char*,char*);
+int same_host_name(char*, char*);
 
 //Link List of Buffers
 struct udp_data send_hdr, recv_hdr;
@@ -38,7 +38,7 @@ struct client_config {
 };
 
 struct buffer *text_buffer;
-int is_EOF = 0;
+volatile int is_EOF = 0;
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -47,38 +47,39 @@ struct client_config cli_config;
 int get_host(char *ip_addr, char *hostname) {
 	char copy[100];
 	strcpy(copy, ip_addr);
-    struct in_addr myservaddr;
-    inet_pton(AF_INET, copy, &myservaddr.s_addr);
-    struct hostent * hostentry = gethostbyaddr(&myservaddr.s_addr, 4, AF_INET);
-    strcpy(hostname, hostentry->h_name);
-    printf("Host name for %s\n", hostentry->h_name);
+	struct in_addr myservaddr;
+	inet_pton(AF_INET, copy, &myservaddr.s_addr);
+	struct hostent * hostentry = gethostbyaddr(&myservaddr.s_addr, 4, AF_INET);
+	strcpy(hostname, hostentry->h_name);
+	printf("Host name for %s\n", hostentry->h_name);
 }
 int main(int argc, char** argv) {
 
-	FILE* fp = fopen("client.in","r");
-	if(fp == NULL){
+	FILE* fp = fopen("client.in", "r");
+	if (fp == NULL) {
 		printf("Cannot open client.in file in READ mode! \n");
 		exit(0);
 	}
-	fscanf(fp,"%s",&cli_config.server_ip);
-	fscanf(fp,"%d",&cli_config.server_port);
-	fscanf(fp,"%s",&cli_config.file_name);
-	fscanf(fp,"%d",&cli_config.rwnd);
-	fscanf(fp,"%d",&cli_config.seed);
-	fscanf(fp,"%f",&cli_config.probability);
-	fscanf(fp,"%d",&cli_config.buffer_read_time);
+	fscanf(fp, "%s", &cli_config.server_ip);
+	fscanf(fp, "%d", &cli_config.server_port);
+	fscanf(fp, "%s", &cli_config.file_name);
+	fscanf(fp, "%d", &cli_config.rwnd);
+	fscanf(fp, "%d", &cli_config.seed);
+	fscanf(fp, "%f", &cli_config.probability);
+	fscanf(fp, "%d", &cli_config.buffer_read_time);
 	log_line_seperator();
 
-	text_buffer = (struct buffer*)malloc(sizeof(struct buffer)*cli_config.rwnd);
+	text_buffer = (struct buffer*) malloc(
+			sizeof(struct buffer) * cli_config.rwnd);
 
 	printf("Client Config Read from client.in file \n");
-	printf("Server IP = %s\n",cli_config.server_ip);
-	printf("Server PORT = %d\n",cli_config.server_port);
-	printf("File Name = %s\n",cli_config.file_name);
-	printf("Sliding Window Size = %d",cli_config.rwnd);
-	printf("Random generator seed = %f\n",cli_config.seed);
-	printf("Drop Probability = %f\n",cli_config.probability);
-	printf("mean u in milliseconds = %d\n",cli_config.buffer_read_time);
+	printf("Server IP = %s\n", cli_config.server_ip);
+	printf("Server PORT = %d\n", cli_config.server_port);
+	printf("File Name = %s\n", cli_config.file_name);
+	printf("Sliding Window Size = %d", cli_config.rwnd);
+	printf("Random generator seed = %f\n", cli_config.seed);
+	printf("Drop Probability = %f\n", cli_config.probability);
+	printf("mean u in milliseconds = %d\n", cli_config.buffer_read_time);
 	log_line_seperator();
 
 	int sockfd;
@@ -96,9 +97,9 @@ int main(int argc, char** argv) {
 	//This is the Server IP that has to be connected.
 	char clihost[40];
 	char servhost[40];
-    get_host(cli_config.server_ip, servhost);
-    get_host(addr[1].ip_addr, clihost);
-	if(strcmp(clihost, servhost) == 0) {
+	get_host(cli_config.server_ip, servhost);
+	get_host(addr[1].ip_addr, clihost);
+	if (strcmp(clihost, servhost) == 0) {
 		printf("Both client and server are on the same host\n");
 		strcpy(server_ip, "127.0.0.1");
 	} else {
@@ -137,7 +138,7 @@ int main(int argc, char** argv) {
 	bzero(&sock_addr, sizeof(sock_addr));
 	socklen_t len = sizeof(struct sockaddr_in);
 	getsockname(sockfd, (SA*) &sock_addr, &len);
-	printf("Client port = %d\n",ntohs(sock_addr.sin_port));
+	printf("Client port = %d\n", ntohs(sock_addr.sin_port));
 
 	Connect(sockfd, (SA*) &servaddr, sizeof(servaddr));
 
@@ -146,7 +147,7 @@ int main(int argc, char** argv) {
 	socklen_t len2 = sizeof(peer_sock);
 
 	getpeername(sockfd, (SA*) &peer_sock, &len2);
-	printf("Server port = %d\n",ntohs(peer_sock.sin_port));
+	printf("Server port = %d\n", ntohs(peer_sock.sin_port));
 
 	char sendline[MAXLINE], recvline[MAXLINE];
 
@@ -176,7 +177,8 @@ int main(int argc, char** argv) {
 	create_new_connection(new_port_number, server_ip);
 	printf("[Client]Done creating connection with new server\n");
 
-	Pthread_join(tid,NULL);
+//	usleep(cli_config.buffer_read_time*3);
+	Pthread_join(tid, NULL);
 
 	close(sockfd);
 	fclose(fp);
@@ -199,11 +201,11 @@ int ispresent(int seq_num) {
 int min_seq_num() {
 	int min_val = INT_MAX;
 	int min_index = 0;
-	int i=0;
+	int i = 0;
 	for (; i < cli_config.rwnd; i++) {
 		if (text_buffer[i].is_filled != -1) {
-			int val = min(min_val,text_buffer[i].seq);
-			if(min_val > val){
+			int val = min(min_val, text_buffer[i].seq);
+			if (min_val > val) {
 				min_index = i;
 				min_val = val;
 			}
@@ -251,6 +253,7 @@ void create_new_connection(int port_num, char* server_ip) {
 
 	int max_seed = cli_config.seed;
 	srand(max_seed);
+//	int eof_seq_no = -1;
 	while (1) {
 		FD_ZERO(&rset);
 		FD_SET(sockfd, &rset);
@@ -269,26 +272,44 @@ void create_new_connection(int port_num, char* server_ip) {
 			}
 
 			pthread_mutex_lock(&mutex);
+//			printf("Lock1 \n");
 
 			if (q_obj.config.type == data) {
-				printf("[Data]Data = %s Received with seq no = %d\n",q_obj.buf,q_obj.config.seq);
+				printf("[Data]Data = %s Received with seq no = %d\n", q_obj.buf,
+						q_obj.config.seq);
 				push_data_to_buffer((char*) &q_obj.buf, q_obj.config.seq,
 						expected_seq_num);
-			} else if(q_obj.config.type == ping) {
+			} else if (q_obj.config.type == ping) {
 				printf("[Ping]Ping Query Received\n");
-			}else if(q_obj.config.type == eof){
-				printf("[EOF]EOFFFFF Received \n");
+			} else if (q_obj.config.type == eof) {
+//				eof_seq_no = q_obj.config.seq;
+				char buf[MAXLINE];
+				bzero(buf,sizeof(buf));
+				strcpy(buf,"End of File!");
+				push_data_to_buffer(&buf, q_obj.config.seq, expected_seq_num);
+				printf("[EOF]EOFFFFF Received for seq no = %d\n",
+						q_obj.config.seq);
 			}
 			//			printf("[Client]Select interrupted in client!!!. Data received = %s\n",q_obj.buf);
 			update_expected_seq_num(&expected_seq_num);
+			printf(
+					"[Ack] Data sent to server with seq no = %d and ack no = %d\n",
+					q_obj.config.seq, expected_seq_num);
 			send_ack_to_server(sockfd, q_obj.sock_addr, sizeof(q_obj.sock_addr),
 					q_obj.config.seq, q_obj.config.ts, expected_seq_num);
 
-			pthread_mutex_unlock(&mutex);
-			if(q_obj.config.type == eof){
+			if (q_obj.config.type == eof
+					&& q_obj.config.seq + 1 == expected_seq_num) {
+				printf("EOFFF actuall happened ! , exiting\n");
 				is_EOF = 1;
 				break;
+			} else {
+//				printf("Expected = %d and got = %d\n", expected_seq_num,
+//						q_obj.config.seq);
 			}
+
+			pthread_mutex_unlock(&mutex);
+
 		}
 	}
 }
@@ -297,17 +318,17 @@ void push_data_to_buffer(char* send_buf, int seq_num, int expected_seq_num) {
 	int i = seq_num % cli_config.rwnd;
 	if (text_buffer[i].is_filled == -1) {
 		if (seq_num != expected_seq_num) {
-//			printf(
-//					"[DROP][DATA]Cannot accept this datagram. Expecting one with seq number %d\n",
-//					expected_seq_num);
+			printf(
+					"[DROP][DATA]Cannot accept this datagram. Expecting one with seq number %d\n",
+					expected_seq_num);
 			return;
 		}
 		strcpy(text_buffer[i].data, send_buf);
 		text_buffer[i].is_filled = 1;
 		text_buffer[i].seq = seq_num;
-//		printf(
-//				"[Data] Pushed to buffer at index %d with data = %s and seq_num = %d\n",
-//				i, send_buf, seq_num);
+		printf(
+				"[Data] Pushed to buffer at index %d with data = %s and seq_num = %d\n",
+				i, send_buf, seq_num);
 	} else {
 //		printf("Buffer is full, dropping datagram with seq num %d\n", seq_num);
 	}
@@ -327,14 +348,15 @@ void* buffer_reader_thread(void* arg) {
 				text_buffer[i].is_filled = -1;
 			}
 		}
-		pthread_mutex_unlock(&mutex);
-		if(is_EOF == 1)
+		if (is_EOF == 1)
 			break;
+//		printf("Lock2 \n");
+		pthread_mutex_unlock(&mutex);
 		usleep(cli_config.buffer_read_time);
+
 	}
 
 }
-
 
 void send_ack_to_server(int sockfd, struct sockaddr_in cliaddr,
 		socklen_t clilen, int seq, uint32_t ts, int expected_seq_num) {
@@ -357,17 +379,17 @@ int get_window_size() {
 	}
 	return count;
 }
-int same_host_name(char* server_ip,char* client_ip){
+int same_host_name(char* server_ip, char* client_ip) {
 	struct in_addr cli_addr_val;
 	struct in_addr serv_addr_val;
 
-	Inet_pton(AF_INET,client_ip,&cli_addr_val.s_addr);
-	Inet_pton(AF_INET,server_ip,&serv_addr_val.s_addr);
+	Inet_pton(AF_INET, client_ip, &cli_addr_val.s_addr);
+	Inet_pton(AF_INET, server_ip, &serv_addr_val.s_addr);
 
 	struct hostent* cli_val;
 	struct hostent* serv_val;
 
-	cli_val = gethostbyaddr(&cli_addr_val.s_addr,4,AF_INET);
-	serv_val = gethostbyaddr(&serv_addr_val.s_addr,4,AF_INET);
+	cli_val = gethostbyaddr(&cli_addr_val.s_addr, 4, AF_INET);
+	serv_val = gethostbyaddr(&serv_addr_val.s_addr, 4, AF_INET);
 	return 0;
 }
